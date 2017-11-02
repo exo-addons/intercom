77999d3b31b70af0c1b6bc713f99a6cfd5611de8
kmenzli <kmenzli@exoplatform.com>
2017-11-02 21:51:14 +0100

    ORG-3358: Create addon to integrate Intercom capabilities (Active on Native Platform bundle)

--- intercom-portlets/src/main/java/org/exoplatform/intercom/respond/package-info.java
+++ intercom-portlets/src/main/java/org/exoplatform/intercom/respond/package-info.java
@@ -2,26 +2,18 @@
 @Portlet(name="RespondPortlet")
 @Scripts(
         {
-                @Script(value = "assets/js/jquery/jquery-3.2.1.min.js", id = "jquery",location = AssetLocation.SERVER),
-                @Script(value = "assets/js/intercom/respond.js",depends = "jquery",location = AssetLocation.SERVER),
-                @Script(value = "assets/js/app.js", location = AssetLocation.SERVER)
+                @Script(value = "js/jquery-3.2.1.min.js", id = "jquery",location = AssetLocation.SERVER),
+                @Script(value = "js/respond.js",id ="respond", depends = "jquery",location = AssetLocation.SERVER)
+
         }
 )
-@Stylesheets(
-        {
-                @Stylesheet(id = "respond", value = "/org/exoplatform/intercom/respond/assets/respond.css", location = AssetLocation.APPLICATION)
-        }
-)
-@Less(value = "respond.less", minify = true)
 
 @Assets("*")
 package org.exoplatform.intercom.respond;
+
 import juzu.Application;
-import juzu.plugin.asset.Assets;
 import juzu.asset.AssetLocation;
+import juzu.plugin.asset.Assets;
 import juzu.plugin.asset.Script;
 import juzu.plugin.asset.Scripts;
-import juzu.plugin.asset.Stylesheet;
-import juzu.plugin.asset.Stylesheets;
-import juzu.plugin.less.Less;
 import juzu.plugin.portlet.Portlet;
